40af299654e9078c91e2516818f097e2056f5004
malithalex <101708349+malithalex@users.noreply.github.com>
2023-08-21 14:25:59 +0530

    contact me upgrade
    added the form with a backend to recieve requests

---
+++ portfolio.php
@@ -0,0 +1,21 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $projectDetails = $_POST["project_details"];
+    
+    $to = "malith.alex@gmail.com"; // Change this to your email address
+    $subject = "New Form Submission: $subject";
+    $message = "Name: $name\nEmail: $email\nProject Details:\n$projectDetails";
+    
+    // Send the email
+    mail($to, $subject, $message);
+
+        if ($sent) {
+        echo "success";
+    } else {
+        echo "error";
+    }
+}
+?>
